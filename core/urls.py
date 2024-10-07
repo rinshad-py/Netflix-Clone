c286e01faa8a7912b68d29acacc41b873a53a134
rinshad-py <rinjurinshad470@gmail.com>
2024-10-08 00:19:45 +0530

    Login, signup

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.home, name='home'),
-   
+   path('', views.index, name='index'),
+   path("login/", views.login, name="login"),
+   path("signup/", views.signup, name="signup"),
+       
 ]
--- netflix/settings.py
+++ netflix/settings.py
@@ -117,7 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Directory where static files will be collected (for production use)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories where Django will search for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
