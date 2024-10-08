42d447c1540c2526342f78885ea92fd0c0ec215c
rinshad-py <rinjurinshad470@gmail.com>
2024-10-08 12:34:08 +0530

    sign completed (responsive)

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
@@ -7,4 +9,29 @@ def login(request):
     return render(request, "login.html")
 
 def signup(request):
-    return render(request, "signup.html")
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email Already Taken!")
+                return redirect("signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username Already Taken!")
+                return redirect("signup")
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                # log user in
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect("/")
+
+        else:
+            messages.info(request, "Password Not Matching!")
+            return redirect("signup")
+
+    else:
+        return render(request, "signup.html")
